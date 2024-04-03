2c3ce494380f94e9ad6147bd0f0ca91baf6f2888
Laabdi <119082919+Laabdi@users.noreply.github.com>
2024-04-03 13:47:59 +0000

    Add files via upload

--- client.c
+++ client.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moaregra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 13:39:22 by moaregra          #+#    #+#             */
+/*   Updated: 2024/04/03 13:39:24 by moaregra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
 int	ft_atoi(char *str)
@@ -24,42 +36,45 @@ int	ft_atoi(char *str)
 	}
 	return (res * s);
 }
-void send_string(char *str,pid_t pid)
-{ 
-    int bits;
-    unsigned char c;
-    while(*str)
-    {
+
+void	send_string(char *str, pid_t pid)
+{
+	int				bits;
+	unsigned char	c;
+
+	while (*str)
+	{
 		bits = 7;
-    	c = *str++;
-		while(bits >= 0)
-    	{
-			if((c >> bits) & 1)
-				kill(pid,SIGUSR1);
+		c = *str++;
+		while (bits >= 0)
+		{
+			if ((c >> bits) & 1)
+				kill(pid, SIGUSR1);
 			else
-			kill(pid,SIGUSR2);
+				kill(pid, SIGUSR2);
 			usleep(100);
 			bits--;
 		}
-    } 
+	}
 }
-int main(int ac,char *av[])
-{
-    int pid;
-    if(ac == 3)
-    {
-        pid = ft_atoi(av[1]);
-        if (pid <= 0)
-        {
-         printf("invalid PID");
-        return 1;
-        }
-        else
-        send_string(av[2],pid);
-        printf("string was sent");
-        return 0;
-        }
-    printf("Invalid input");
-    return(0);
 
+int	main(int ac, char *av[])
+{
+	int	pid;
+
+	if (ac == 3)
+	{
+		pid = ft_atoi(av[1]);
+		if (pid <= 0)
+		{
+			printf("invalid PID");
+			return (1);
+		}
+		else
+			send_string(av[2], pid);
+		printf("string was sent");
+		return (0);
+	}
+	printf("Invalid input");
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,38 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moaregra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 13:44:11 by moaregra          #+#    #+#             */
+/*   Updated: 2024/04/03 13:44:12 by moaregra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
-
-void    bit_writer(int bit_recieved)
+void	bit_writer(int bit_recieved)
 {
-    static int bit = 7;
-    static int set = 0;
-    set += (bit_recieved << bit);
-    // printf("bit value after bit shifting :%d\n",bit);
-    // printf("set value after bit shifting :%d\n",set);
-    if (bit == 0)
-    {
-        printf("%c",set);
-        bit = 7;
-        set = 0;
-    }
-    else
-        bit--;
-}
-void    bit_handler(int bit_recieved)
-{
-    if(bit_recieved == SIGUSR1)
-    bit_writer(1);
-    else
-    bit_writer(0);
+	static int	bit;
+	static int	set;
 
+	bit = 7;
+	set = 0;
+	set += (bit_recieved << bit);
+	if (bit == 0)
+	{
+		printf("%c", set);
+		bit = 7;
+		set = 0;
+	}
+	else
+		bit--;
 }
-int main(void)
-{  
-    int nb = getpid();
-    printf("server PID ----->%u\n",nb);
-    signal(SIGUSR1,bit_handler);
-    signal(SIGUSR2,bit_handler);
-    printf("signal sent by the client ----->");
-    while(1)
-    pause();
+
+void	bit_handler(int bit_recieved)
+{
+	if (bit_recieved == SIGUSR1)
+		bit_writer(1);
+	else
+		bit_writer(0);
 }
-  
+
+int	main(void)
+{
+	int	nb;
+
+	nb = getpid();
+	printf("server PID ----->%u\n", nb);
+	signal(SIGUSR1, bit_handler);
+	signal(SIGUSR2, bit_handler);
+	printf("signal sent by the client ----->");
+	while (1)
+		pause();
+}
